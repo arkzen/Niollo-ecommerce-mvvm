2ba47bda4541e2b0aa164e352d01b2e1177c3998
Asikur <asikurr817@gmail.com>
2023-06-04 23:55:40 +0600

    updated

---
+++ app/src/main/java/studios/darkzen/niollo/view/activity/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package studios.darkzen.niollo.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import studios.darkzen.niollo.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
--- app/src/main/java/studios/darkzen/niollo/view/activity/ShoppingActivity.kt
+++ app/src/main/java/studios/darkzen/niollo/view/activity/ShoppingActivity.kt
@@ -1,11 +1,12 @@
-package studios.darkzen.niollo
+package studios.darkzen.niollo.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import studios.darkzen.niollo.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
