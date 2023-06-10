d33bc40bc19a6c3e6363c468d05931b084e12c3d
Asikur <asikurr817@gmail.com>
2023-06-10 23:52:25 +0600

    fragment introduction added

---
+++ app/src/main/java/studios/darkzen/niollo/view/fragments/loginRegister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package studios.darkzen.niollo.view.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import studios.darkzen.niollo.R
+
+class IntroductionFragment : Fragment(R.layout.fragment_introduction) {
+}
