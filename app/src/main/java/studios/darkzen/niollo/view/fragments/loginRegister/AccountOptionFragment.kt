214861beaba4b2eea2e8586b33764e9c4fe24c4d
Asikur <asikurr817@gmail.com>
2023-06-10 23:52:07 +0600

    fragment account option added

---
+++ app/src/main/java/studios/darkzen/niollo/view/fragments/loginRegister/AccountOptionFragment.kt
@@ -0,0 +1,7 @@
+package studios.darkzen.niollo.view.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import studios.darkzen.niollo.R
+
+class AccountOptionFragment : Fragment(R.layout.fragment_account_option) {
+}
