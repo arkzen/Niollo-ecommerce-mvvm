dc44f0193820ad45db396eb885d10e0ed04d647b
Asikur <asikurr817@gmail.com>
2023-06-10 23:52:58 +0600

    fragment Register added

---
+++ app/src/main/java/studios/darkzen/niollo/view/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,9 @@
+package studios.darkzen.niollo.view.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import studios.darkzen.niollo.R
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+
+}
